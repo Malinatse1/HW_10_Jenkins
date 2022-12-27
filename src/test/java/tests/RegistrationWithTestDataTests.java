3f2425d56eeee8c51d90d1a129ab7721f0105bd2
Natalya Bochkova <bochkovanatalya7@gmail.com>
2022-12-27 23:43:25 +0300

    add Attach

--- src/test/java/tests/RegistrationWithTestDataTests.java
+++ src/test/java/tests/RegistrationWithTestDataTests.java
@@ -1,7 +1,12 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -17,6 +22,10 @@ public class RegistrationWithTestDataTests extends TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+    }
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
 
@@ -80,5 +89,12 @@ public class RegistrationWithTestDataTests extends TestBase {
                 .verifyResult("State and City", state + " " + city);
 
 
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
